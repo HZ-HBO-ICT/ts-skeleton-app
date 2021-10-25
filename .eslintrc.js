046796c4add576c5a82f1d53a09fcd75e8a16622
Loek van der Linde <lind0096@hz.nl>
2021-10-25 16:22:33 +0200

    Add ESLint with sane TypeScript defaults

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import'
+  ],
+  extends: [
+    'airbnb-typescript/base',
+  ],
+  parserOptions: {
+      project: './tsconfig.json',
+  }
+};

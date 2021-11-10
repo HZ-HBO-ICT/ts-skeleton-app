4129d8d13a06f0d35661d48f13d39607d984d578
Daan de Waard <daan.de.waard@hz.nl>
2021-11-10 16:20:28 +0100

    Add JSDocs rules to linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,39 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
-    'import'
+    'import',
+    'jsdoc'
   ],
   extends: [
     'airbnb-base',
     'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsdoc/recommended'
   ],
   parserOptions: {
       project: './tsconfig.json',
   },
   rules: {
     'no-console': 0,
+    'jsdoc/require-jsdoc': [
+      'error', 
+      {
+        'contexts': [
+          'MethodDefinition:not([accessibility="private"])'
+        ],
+        'require': {
+          'ClassDeclaration': false,
+          'MethodDefinition': false
+        },
+      }
+    ],
+    'jsdoc/require-param-type': 0,
+    'jsdoc/no-types': [
+      'warn',
+      {
+        'contexts': ['any']
+      } 
+    ],
     'camelcase': 'off',
     '@typescript-eslint/naming-convention': [
       'error',
